014d08a4ec31219737a969f4f7a7c245f5f21d9a
Javed Ahmadzai <theahmadzai@hotmail.com>
2021-01-02 05:47:32 +0500

    listen to notifications via subscriptions

--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -1,11 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { Alert, FlatList } from 'react-native'
+import { Container, ListItem, Text, Body } from 'native-base'
+import { gql, useSubscription } from '@apollo/client'
+import Loading from '../components/Loading'
+
+const NOTIFICATION_SUBSCRIPTION = gql`
+  subscription {
+    notification {
+      title
+    }
+  }
+`
 
 const NotificationsScreen = () => {
+  const [notifications, setNotifications] = useState([])
+
+  const { loading, error } = useSubscription(NOTIFICATION_SUBSCRIPTION, {
+    async onSubscriptionData({ subscriptionData: { data } }) {
+      setNotifications([...notifications, data.notification.title])
+    },
+  })
+
+  if (loading) return <Loading />
+  if (error) {
+    Alert.alert(error.name, error.message)
+  }
+
   return (
-    <View>
-      <Text>Notifications</Text>
-    </View>
+    <Container>
+      <FlatList
+        data={notifications}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => (
+          <ListItem>
+            <Body>
+              <Text>{item}</Text>
+            </Body>
+          </ListItem>
+        )}
+      />
+    </Container>
   )
 }
 
