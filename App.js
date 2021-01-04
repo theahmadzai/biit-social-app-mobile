2f1fa8718565f08212bc280eb68ba4ed1fbcd3db
Javed Ahmadzai <theahmadzai@hotmail.com>
2021-01-05 02:21:50 +0500

    move graphql client to separate file

--- App.js
+++ App.js
@@ -3,53 +3,13 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { useFonts } from 'expo-font'
 import { Ionicons } from '@expo/vector-icons'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  ApolloLink,
-  split,
-} from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-import { createUploadLink } from 'apollo-upload-client'
-import { WebSocketLink } from '@apollo/client/link/ws'
-import { getMainDefinition } from '@apollo/client/utilities'
+import { ApolloProvider } from '@apollo/client'
 import { Provider as PaperProvider } from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native'
-import { getAuthToken } from './src/utils'
+import client from './src/client'
 import { AuthProvider } from './src/contexts/AuthContext'
 import InitialScreen from './src/screens/InitialScreen'
 import SplashScreen from './src/screens/SplashScreen'
-
-const API_URL = 'http://192.168.1.2:3000/graphql'
-
-const client = new ApolloClient({
-  link: split(
-    ({ query }) => {
-      const definition = getMainDefinition(query)
-      return (
-        definition.kind === 'OperationDefinition' &&
-        definition.operation === 'subscription'
-      )
-    },
-    new WebSocketLink({
-      uri: `ws://192.168.1.2:3000/graphql`,
-      options: {
-        reconnect: true,
-        async connectionParams() {
-          return { authToken: await getAuthToken() }
-        },
-      },
-    }),
-    ApolloLink.from([
-      setContext(async (_, { headers }) => ({
-        headers: { ...headers, authorization: await getAuthToken() },
-      })),
-      createUploadLink({ uri: API_URL }),
-    ])
-  ),
-  cache: new InMemoryCache(),
-})
 
 const App = () => {
   const [loaded] = useFonts({
---
+++ src/client.js
@@ -0,0 +1,49 @@
+import { ApolloClient, InMemoryCache, ApolloLink, split } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { onError } from '@apollo/client/link/error'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { createUploadLink } from 'apollo-upload-client'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { getAuthToken } from './utils'
+
+const API_URL = 'http://192.168.1.2:3000/graphql'
+
+const client = new ApolloClient({
+  link: split(
+    ({ query }) => {
+      const definition = getMainDefinition(query)
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      )
+    },
+    new WebSocketLink({
+      uri: `ws://192.168.1.2:3000/graphql`,
+      options: {
+        reconnect: true,
+        async connectionParams() {
+          return { authToken: await getAuthToken() }
+        },
+      },
+    }),
+    ApolloLink.from([
+      setContext(async (_, { headers }) => ({
+        headers: { ...headers, authorization: await getAuthToken() },
+      })),
+      createUploadLink({ uri: API_URL }),
+    ]),
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        )
+
+      if (networkError) console.log(`[Network error]: ${networkError}`)
+    })
+  ),
+  cache: new InMemoryCache(),
+})
+
+export default client
