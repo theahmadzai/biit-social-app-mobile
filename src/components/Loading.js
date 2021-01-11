81e7b62fdd4685647c92504ea909bedad8f9cbb4
Javed Ahmadzai <theahmadzai@hotmail.com>
2021-01-11 19:02:13 +0500

    change ActivityIndicator color

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
 
 const Loading = props => (
   <View style={styles.container} {...props}>
-    <ActivityIndicator size="large" color="teal" />
+    <ActivityIndicator size="large" color="#008e50" />
   </View>
 )
 
