2fca1d53a09b11e775e37ba888480b96b7997c7b
Muhammad Javed <theahmadzai@hotmail.com>
2021-02-08 05:41:43 +0500

    class wall screen

---
+++ src/screens/WallScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Text } from 'native-base'
+
+const WallScreen = () => {
+  return (
+    <Container>
+      <Text>Class Wall</Text>
+    </Container>
+  )
+}
+
+export default WallScreen
